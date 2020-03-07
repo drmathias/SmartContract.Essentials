cbc27da53c1f7637ee00c02944dc98501f0c6b7b
Adam Shirt <adamshirt@developmomentum.com>
2020-03-07 16:17:52 +0000

    Include Sha3 hashing library

---
+++ src/SmartContract.Essentials/Hashing/Sha3Extensions.cs
@@ -0,0 +1,18 @@
+﻿using SHA3.Net;
+using System.Text;
+
+namespace SmartContract.Essentials.Hashing
+{
+    public static class Sha3Extensions
+    {
+        /// <summary>
+        /// Computes the hash value for the specified string
+        /// </summary>
+        /// <exception cref="System.ArgumentNullException"></exception>
+        /// <exception cref="System.ObjectDisposedException"></exception>
+        public static byte[] ComputeHash(this Sha3 hasher, string value)
+        {
+            return hasher.ComputeHash(Encoding.UTF8.GetBytes(value));
+        }
+    }
+}
